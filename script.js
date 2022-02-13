d133e7e0ad7544fdf62cce3a304e4bb56feffa2c
눈보라 <83201165+zenitsu224@users.noreply.github.com>
2022-02-13 21:05:25 +0900

    이계 혁명 연합

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function printName()  {
-    const name = document.getElementById('name').value;
+var idpw = {"hoochoo":"kojw0224", "jsjsjs":"js123123", "262mee":"262seoseo"};
+//idpw[0] = 후루추룹
+//idpw[1] = 정시니
+//idpw[2] = 메밀묵
+function login()  {
+    var id = document.getElementById("id").value;
+    var pw = document.getElementById("pw").value;
+    
+    if(id in idpw && idpw[id] == pw) {
+        location.href = "index.html";
+    }
+    else{
+        alert("아이디나 비밀번호가 틀렸습니다");
+    }
 }
-
